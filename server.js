070d3e7a3d739de5eda41fc885bdd85566db73c0
Denisa Dupu <dupudenisa@gmail.com>
2020-04-16 03:35:40 -0500

    Allowed production build to cease redirection to index.html page

--- server.js
+++ server.js
@@ -20,10 +20,6 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(routes);
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './client/build/index.html'));
-});
-
 db.sequelize.sync({force: true}).then(() => {
   console.log("Drop and Resync DB");
 

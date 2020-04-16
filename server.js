177febbf98a30ff54691d889bfa038b6fbef30b2
Denisa Dupu <dupudenisa@gmail.com>
2020-04-16 02:51:06 -0500

    Allowed production build to cease redirection to index.html page

--- server.js
+++ server.js
@@ -14,9 +14,13 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
 }
+
 
 app.use(routes);
 

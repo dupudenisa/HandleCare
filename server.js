223a643488f7a9d8185e88fbbbac514cc76bdbd8
Denisa Dupu <dupudenisa@gmail.com>
2020-04-15 22:09:35 -0500

    Comments removed, serverside updates

--- server.js
+++ server.js
@@ -14,11 +14,19 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+app.use(express.static(path.join(__dirname, 'client/build')));
 
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  //
+  app.get('*', (req, res) => {
+    res.sendfile(path.join(__dirname = 'client/build/index.html'));
+  })
+}
+//build mode
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/public/index.html'));
+})
 app.use(routes);
 
 db.sequelize.sync({force: true}).then(() => {

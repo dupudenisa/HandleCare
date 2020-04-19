c3fc0b281b95dcc801090b58b2eba734695e13ff
Denisa Dupu <dupudenisa@gmail.com>
2020-04-19 18:51:14 -0500

    logo

--- client/src/components/pages/dashboard.js
+++ client/src/components/pages/dashboard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Logo from "../assets/logo.png"
 
 export default function Dashboard() {
     return (
@@ -6,7 +7,7 @@ export default function Dashboard() {
         <div>
 
 
-           <h1 style={{ textAlign: 'center',alignSelf: 'center'}}> Welcome to HandleCare! </h1>
+           <h1 style={{ textAlign: 'center',alignSelf: 'center'}}> <img src={Logo} className="card-img" alt="myimg" /> </h1>
 
 
         </div>
